5575fd717b83e6d76eaf9171922b4c310a799543
Whyetzz <75259101+WhyEtzz@users.noreply.github.com>
2024-12-19 22:47:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve("acumalaka.html"))
 })
 
+app.get("/kodok", (req, res)=>{
+    res.sendFile(path.resolve("kodok-acumalaka.gif"))
+})
+
 app.get("/translate/:lang/:input", async(req, res)=> {
     if(req.params.lang &&  req.params.input) {
     const translated = await translate(req.params.input, {to: req.params.lang})
-    console/log(`From: ${req.params.input} \n To: ${translated}`)
+    console.log(`From: ${req.params.input} \n To: ${translated}`)
     res.status(200).send(translated)
 } else{
     res.status(400).send("Invalid request")
